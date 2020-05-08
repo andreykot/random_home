3f8c1ffd1b20198ff6ec942ff5507854fb1bc5bf
andreykot <kotovskyandrew@yandex.ru>
2020-05-09 01:32:20 +0300

    Apartment:  - init workable create_link method

--- cian_parser.py
+++ cian_parser.py
@@ -14,11 +14,12 @@ Price = namedtuple('Price', ['min', 'max'])
 class Apartment:
     """Class to initialize apartment with his special parameters."""
 
-
-    def __init__(self, deal: tuple, rooms: tuple, apartment_type: tuple = (True, True), price: tuple = (None, None),
-                 ):
+    def __init__(self, region: str, deal: tuple, rooms: tuple = (1, 2), apartment_type: tuple = (True, True),
+                 price: tuple = (None, None)):
         """
         Args:
+            region (:str): Name of region for search.
+                Example: Saint Petersburg.
             deal (:tuple of :bool): Deal type of apartment. There are four boolean options that can be combined:
                 for sale or for rent 1year+, rent 1year- or per-day rent.
                 Example: (False, True, True) -  apartment's deal type is only for rent and per-day rent.
@@ -29,31 +30,73 @@ class Apartment:
             price (:tuple of :int or Nonetype): Min and max price of apartment.
                 Example: (None, 3000000) - min price is None, max price is 3000000.
         """
+
+        self.region = region
         self.deal = Deal(*deal)
         self.rooms = rooms
         self.apartment_type = ApartmentType(*apartment_type)
         self.price = Price(*price)
+        self.region = region
+
+    def create_link(self):
+        query = r'https://cian.ru/cat.php?'
+        for func in self.get_deal, self.get_rooms, self.get_apartment_type, self.get_price:
+            query = func(query)
+        return query
 
     def get_deal(self, query):
         if self.deal.sale and (self.deal.rent_yearplus or self.deal.rent_yearminus or self.deal.rent_per_day):
             raise ValueError('Incompatible arguments for object Deal.')
 
         if self.deal.sale:
-            return 'deal_type=sale' + '&' + query
+            return query + '&' + 'deal_type=sale'
         else:
             if self.deal.rent_yearplus:
-                return 'deal_type=rent' + '&' + query + '&' + 'type=4'
+                return query + '&' + 'deal_type=rent' + '&' + 'type=4'
             elif self.deal.rent_yearminus:
-                return 'deal_type=rent' + '&' + query + '&' + 'type=3'
+                return query + '&' + 'deal_type=rent' + '&' + 'type=3'
             elif self.deal.rent_per_day:
-                return 'deal_type=rent' + '&' + query + '&' + 'type=2'
+                return query + '&' + 'deal_type=rent' + '&' + 'type=2'
             else:
                 raise ValueError('No arguments for deal type.')
 
-    def create_link(self):
-        #t = r'https://spb.cian.ru/cat.php?deal_type=sale&engine_version=2&offer_type=flat&room1=1&room2=1'
-        pass
+    def get_rooms(self, query):
+        for n in list(self.rooms):
+            if isinstance(n, int) and n > 0:
+                query += '&' + 'room{}=1'.format(n)
+            else:
+                raise ValueError('Incompatible type of arguments for "rooms". Need tuple of int objects.')
+        return query
+
+    def get_apartment_type(self, query):
+        if not self.deal.sale or (self.apartment_type.new and self.apartment_type.secondary):
+            return query
+        elif self.apartment_type.new:
+            return query + '&' + 'object_type=2'
+        elif self.apartment_type.secondary:
+            return query + '&' + 'object_type=1'
+        else:
+            raise RuntimeError
+
+    def get_price(self, query):
+        if isinstance(self.price.min, int) and self.price.min <= self.price.max:
+            query += '&' + 'minprice=' + str(self.price.min)
+
+        if isinstance(self.price.max, int) and self.price.min <= self.price.max:
+            query += '&' + 'maxprice=' + str(self.price.max)
+
+        return query
+
+    def get_region(self, query):
+        if self.region == 'Moscow':
+            query += '&' + 'region=1'
+        elif self.region == 'Saint Petersburg':
+            query += '&' + 'region=2'
+        else:
+            raise ValueError('Unsupported region.')
 
 
 if __name__ == '__main__':
-    pass
+    ap = Apartment(deal=(False, False, True, False), price=(0, 49900), apartment_type=(True, False))
+    link = ap.create_link()
+    print(link)
