855c7d2cbb2621e8111904f30c2616e71e75d420
andreykot <kotovskyandrew@yandex.ru>
2020-05-27 05:36:40 +0300

    minor run.py

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
 from aiogram import executor
+import logging
 
-from app import bot
+from app import bot_init
+from app.db.utils import init_db
 
 
 def main():
-    executor.start_polling(bot.dispatcher, skip_updates=True)
+    #logging.basicConfig(level=logging.DEBUG)
+    executor.start_polling(bot_init.dispatcher, skip_updates=True)
 
 
 if __name__ == '__main__':
+    #init_db()
     main()
