24f6f81d01e0e486d3e4f19ee7ce807dc29340d0
andreykot <kotovskyandrew@yandex.ru>
2020-05-27 05:33:02 +0300

    dev db     Добавил новую таблицу UserSettings, разобрался с join'ами в sqlalchemy, добавил требуемые методы в классы таблиц. В utils.py добавил функцию-обертку для безопасных коммитов в БД.

--- app/db/db_map.py
+++ app/db/db_map.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Table
+from sqlalchemy import update, delete
 from sqlalchemy import create_engine, Column, Integer, String, DateTime, Boolean, Float, ARRAY, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session, relationship
@@ -12,53 +12,72 @@ DB = 'postgresql+psycopg2://{}:{}@{}/random_home_test'.format(RANDOM_HOME_DB_USE
                                                               RANDOM_HOME_DB_PASSWORD,
                                                               DB_SERVER_IP)  #TODO test db
 Base = declarative_base()
+engine = create_engine(DB)
 
 
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    telegram_id = Column(String)
+    telegram_id = Column(Integer)
     last_use = Column(DateTime)
-    query = relationship("Query")
+    queries = relationship("Query", back_populates="user")
+    settings = relationship("UserSettings", uselist=False, back_populates="user")
 
     def __init__(self, telegram_id, last_use):
         self.telegram_id = telegram_id
         self.last_use = last_use
 
     def __repr__(self):
-        return "<User({self.telegram_id}, {self.last_use})>"
+        return f"<User({self.telegram_id}, {self.last_use})>"
 
     @classmethod
-    def add_user(cls, engine, telegram_id):
-        add_to_database(engine, User(telegram_id, datetime.now()))
+    def insert_user(cls, telegram_id):
+        session = Session(bind=engine)
+        user = User(telegram_id, datetime.now())
+        session.add(user)
+        session.commit()
+        session.refresh(user)
+
+        session = Session(bind=engine)
+        query = Query(user.id)
+        session.add(query)
+        session.commit()
+        session.refresh(query)
+
+        session.add(UserSettings(user.id, editing_query=query.id))
+        session.commit()
+        session.close()
+
+        session = Session(bind=engine)
+        print(session.query(User).all())
+        print(session.query(Query).all())
+        print(session.query(UserSettings).all())
 
     @classmethod
-    def get_users(cls, engine):
+    def get_users(cls):
         session = Session(bind=engine)
         return session.query(User).all()
 
     @classmethod
-    def is_user(cls, engine, telegram_id):
+    def is_user(cls, telegram_id):
         session = Session(bind=engine)
-        result = session.query(User.telegram_id == telegram_id)
-        if result:
-            return True
-        else:
-            return False
+        result = session.query(User).filter(User.telegram_id == telegram_id).all()
+        return True if result else False
 
 
 class Query(Base):
     __tablename__ = 'queries'
     id = Column(Integer, primary_key=True)
-    user = Column(Integer, ForeignKey('users.id'))
+    user_id = Column(Integer, ForeignKey('users.id'))
     region = Column(String)
     deal = Column(ARRAY(Integer))
     rooms = Column(ARRAY(Integer))
     apartment_type = Column(String)
     price = Column(ARRAY(Float))
+    user = relationship('User', back_populates='queries', uselist=False)
 
-    def __init__(self, user, region, deal, rooms, apartment_type, price):
-        self.user = user
+    def __init__(self, user_id, region=None, deal=None, rooms=None, apartment_type=None, price=None):
+        self.user_id = user_id
         self.region = region
         self.deal = deal
         self.rooms = rooms
@@ -66,14 +85,54 @@ class Query(Base):
         self.price = price
 
     def __repr__(self):
-        return "<Query({self.user}, {self.region}, {self.deal}, {self.rooms}, {self.apartment_type}, {self.price})>"
+        return f"<Query({self.user_id}, {self.region}, {self.deal}, {self.rooms}, {self.apartment_type}, {self.price})>"
+
+
+class UserSettings(Base):
+    """
+    user_id: int
+        User.id
+    current_edit_msg: int.
+        Telegram message id of current message with main search terms.
+    editing_query: int
+        Queries.id of last user's query.
+    """
+    __tablename__ = 'user_settings'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    current_edit_msg = Column(Integer)
+    editing_query = Column(Integer)
+    user = relationship('User', back_populates='settings', uselist=False)
+
+    def __init__(self, user_id, current_edit_msg=None, editing_query=None):
+        self.user_id = user_id
+        self.current_edit_msg = current_edit_msg
+        self.editing_query = editing_query
+
+    def __repr__(self):
+        return f"<UserSettings({self.user_id}, {self.current_edit_msg}, {self.editing_query})>"
 
     @classmethod
-    def add_query_for_user(cls, engine, user, region, deal, rooms, apartment_type, price):
-        add_to_database(engine, Query(user, region, deal, rooms, apartment_type, price))
+    def update_current_edit_msg(cls, telegram_id, message_id):
+        session = Session(bind=engine)
+        try:
+            user = session.query(User).filter(User.telegram_id == telegram_id).one()
+            user.settings.current_edit_msg = message_id
+            session.commit()
+        except Exception:
+            print(traceback.format_exc())
+            session.rollback()
+        finally:
+            session.close()
+
+    @classmethod
+    def get_current_edit_msg(cls, telegram_id):
+        session = Session(bind=engine)
+        user = session.query(User).filter(User.telegram_id == telegram_id).one()
+        return user.settings.current_edit_msg
 
 
-def add_to_database(engine, data):
+def insert_to_db(data):
     session = Session(bind=engine)
     try:
         session.add(data)
--- app/db/utils.py
+++ app/db/utils.py
@@ -15,9 +15,23 @@ def send_sql_query(query: str, get_result: bool):
     pass
 
 
+def safe_commit(session):
+    try:
+        session.commit()
+    except Exception:
+        print(traceback.format_exc())
+        session.rollback()
+    finally:
+        session.close()
+
+
 def init_db():
     engine = create_engine(DB)
     Base.metadata.create_all(engine)
+
+
+def drop_db():
+    pass
 
 
 if __name__ == '__main__':
